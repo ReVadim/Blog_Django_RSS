f3d9f9f6adabacc78ada4b22842cace51a3faf15
ReVadim <revani@yandex.ru>
2022-12-26 00:05:37 +0300

    add pages pagination with EmtyPage and PageNotAnInteger

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
     """ display all posts
     """
-    posts = Post.published.all()
-
+    all_posts = Post.published.all()
+    paginator = Paginator(all_posts, 3)
+    page_number = request.GET.get('page')
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+        
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
