94c581d5ea32a6b059770580ab57569bea7bf6ae
ReVadim <revani@yandex.ru>
2022-12-27 00:13:15 +0300

    add google gmail and smtp settings

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,22 +1,36 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
+"""
 def post_list(request):
-    """ display all posts
-    """
+    ''' display all posts
+    '''
     all_posts = Post.published.all()
     paginator = Paginator(all_posts, 3)
     page_number = request.GET.get('page')
     try:
-        posts = paginator.page(page_number)
+        posts = paginator.page(page_number) 
     except PageNotAnInteger:
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-        
+
     return render(request, 'blog/post/list.html', {'posts': posts})
+"""
+
+
+class PostListView(ListView):
+    """ Class-based view to list posts
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    template_name = 'blog/post/list.html'
+    paginate_by = 3
 
 
 def post_detail(request, year, month, day, post):
@@ -32,3 +46,25 @@ def post_detail(request, year, month, day, post):
                              )
 
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    """ Published post by id
+    """
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{data['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{data['name']}\'s comments: {data['comments']}"
+            send_mail(subject, message, 'revani.web@gmail.com', [data['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
