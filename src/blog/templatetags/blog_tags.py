a6aaa2a67464deee6e69c3d7eecaeb57c079ff8f
ReVadim <revani@yandex.ru>
2023-01-03 23:12:33 +0300

    add custom markdown filter

--- src/blog/templatetags/blog_tags.py
+++ src/blog/templatetags/blog_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from ..models import Post
-
+from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
 
@@ -19,3 +21,17 @@ def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
 
     return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    """ Display tag with most commented posts
+    """
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    """ Custom filter to use Markdown syntax in blog posts
+    """
+    return mark_safe(markdown.markdown(text))
